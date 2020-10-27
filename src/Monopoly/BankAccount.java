e2cddb923a67e17122342bab0fc1ddb64c2906e1
Thamara Chellakooty <thamaralinnea@gmail.com>
2020-10-27 11:42:32 +0100

    class BankAccount created and methods regarding BankAccount

---
+++ src/Monopoly/BankAccount.java
@@ -0,0 +1,21 @@
+package Monopoly;
+
+public class BankAccount {
+    int balance;
+
+    // set balance to startbalance
+    public BankAccount () {
+        balance = 1000;
+    }
+    // get square value from player nad find new balance
+    public void getSquareValue (int newSqaure) {
+        balance = balance + newSqaure;
+
+    }
+     // return balance to monopolygame
+    public int getBalance () {
+        return balance;
+    }
+}
+
+
