35deb239f84d77495b3759283719b2d47964bdec
Pernille <pernille.lyngholm1@outlook.com>
2020-10-27 12:10:03 +0100

    Setters and Getters in Player class

--- src/Monopoly/Player.java
+++ src/Monopoly/Player.java
@@ -1,4 +1,23 @@
 package Monopoly;
 
 public class Player {
+
+    public String name;
+    public int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
