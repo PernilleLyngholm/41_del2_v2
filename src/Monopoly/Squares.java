0f1a3801edf68c9e099580990339d0657f7c4fd5
cboej <s205360@student.dtu.dk>
2020-10-28 09:20:24 +0100

    Square class done

---
+++ src/Monopoly/Squares.java
@@ -0,0 +1,25 @@
+package Monopoly;
+
+public class Squares {
+    private String description;
+    private int value;
+
+    private Boolean bonusturn;
+
+    public Squares (String description, int value, Boolean bonusturn){
+        this.description = description;
+        this.value = value;
+
+    }
+    public String getDescription(){
+        return description;
+
+    }
+    public int getValue(){
+        return value;
+
+    }
+    public Boolean getBonusturn(){
+        return bonusturn;
+    }
+}
