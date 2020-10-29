2c7009216b16a00dd5b460128f3a96dde7ab0736
M-Najib Hebrawi <najiphebrawi@hotmail.com>
2020-10-29 11:54:53 +0100

    1.1.1.1.1

---
+++ src/Monopoly/GuiGame.java
@@ -0,0 +1,27 @@
+
+
+ /*
+    import gui_fields.GUI_Field;
+import gui_fields.GUI_Player;
+import gui_main.GUI;
+
+    public class GuiGame {
+
+
+        public static void main(String[] args) {
+            GUI gui=new GUI();
+
+
+            GUI_Player p1=new GUI_Player("Najib ");
+            GUI_Player p2=new GUI_Player("Nana");
+            gui.addPlayer(p1);
+            gui.addPlayer(p2);
+
+            GUI_Field gf[]= gui.getFields();
+            System.out.println(gf[0].getTitle());
+
+        }
+    }
+
+
+*/
