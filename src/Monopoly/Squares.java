f28438545923f180472c8073a2dfe7b10dd13f38
cboej <s205360@student.dtu.dk>
2020-10-30 14:46:30 +0100

    squares done

--- src/Monopoly/Squares.java
+++ src/Monopoly/Squares.java
@@ -3,12 +3,12 @@ package Monopoly;
 public class Squares {
     private String description;
     private int value;
-
     private Boolean bonusturn;
 
     public Squares (String description, int value, Boolean bonusturn){
         this.description = description;
         this.value = value;
+        this.bonusturn = bonusturn;
 
     }
     public String getDescription(){
@@ -20,6 +20,7 @@ public class Squares {
 
     }
     public Boolean getBonusturn(){
+
         return bonusturn;
     }
 }
