083a83453ecae00bbfffa1a9c57c049318582096
Thamara Chellakooty <thamaralinnea@gmail.com>
2020-10-30 14:29:54 +0100

    Methods to BankAccount added and int balance changed to account

--- src/Monopoly/BankAccount.java
+++ src/Monopoly/BankAccount.java
@@ -1,21 +1,30 @@
 package Monopoly;
 
 public class BankAccount {
-    int balance;
+    int account;
+    static int bottomBalance = 0;
 
     // set balance to startbalance
     public BankAccount () {
-        balance = 1000;
+        account = 1000;
     }
     // get square value from player nad find new balance
 
-    public void getSquareValue (int newSqaure) {
-        balance = balance + newSqaure;
+    public void setAccount (int account) {
+        this.account = account;
+
+    }
+
+    public void addPoints ( int squareValue){
+        account += squareValue;
+        if (account<bottomBalance){
+            account = bottomBalance;
+        }
 
     }
      // return balance to monopolygame
     public int getBalance () {
-        return balance;
+        return account;
     }
 }
 
