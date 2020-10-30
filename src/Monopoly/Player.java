fc990d03087b44d19ff44dca42080336e2af3090
Thamara Chellakooty <thamaralinnea@gmail.com>
2020-10-30 14:37:55 +0100

    Player class modified

--- src/Monopoly/Player.java
+++ src/Monopoly/Player.java
@@ -3,32 +3,20 @@ package Monopoly;
 public class Player {
 
     public String name;
-    public int age;
     public BankAccount account;
 
-    public Player (String name, int age, BankAccount account){
+    public Player (){
         this.name = name;
-        this.age = age;
         this.account = new BankAccount();
     }
 
-    public void setName(String name) {
+    public void setName(String name){
         this.name = name;
     }
-
-    public String getName() {
+    public String getName (){
         return name;
     }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-
 }
+
 
 
