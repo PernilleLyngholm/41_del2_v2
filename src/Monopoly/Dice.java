3e88eea055cf9dfec75ecbbcc842a7bc334f4614
cboej <s205360@student.dtu.dk>
2020-10-30 14:35:58 +0100

    dice done

--- src/Monopoly/Dice.java
+++ src/Monopoly/Dice.java
@@ -3,25 +3,37 @@ package Monopoly;
 import java.util.Random;
 
 public class Dice {
+    //giver terning 6 sider
+private int Max = 6;
+private facevalue;
 
-    //her laver vi en a variabel
-    private int numberOfside;
+public Dice(int value, int facevalue){
+    facevalue = value;
 
-    // her laver vi a construktor
-    public Dice(){
-        this.numberOfside=6;
-    }
-    //her er en anden construktor
-    public Dice(int s){
-        if(s>1)
-            this.numberOfside=s;
-    }
-    // her laver vi en method/ funktion som har et opgave at rolle terning og viser den numberofside
-    public int rolle(){
+}
+public Dice (){
+    //kast terningen og vis værdien.
+    public int roll(){
+        //+1 for at terningen viser 6
+    facevalue = (int)
+            (Math.random()*Max)+1;
 
-        Random rand= new Random(); // den her hadder en objekt
-        return 1+rand.nextInt((numberOfside));
+    return facevalue;
+
+
     }
+    //hent værdien
+    public int getFacevaule(){
+        return facevalue;
+
+    }
+    //retunerer en strengrrpræsentation og objektes data.
+    public String toString(){
+        String result = "value" + facevalue;
+        return result;
+
+    }
+}
 
 
 
