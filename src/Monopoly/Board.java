8a26225576170b20fc6cca485ee55b708de6a28d
Pernille <pernille.lyngholm1@outlook.com>
2020-10-30 14:37:39 +0100

    Board finished

--- src/Monopoly/Board.java
+++ src/Monopoly/Board.java
@@ -2,26 +2,44 @@ package Monopoly;
 
 public class Board {
     Squares[] squares = new Squares[11]; {
-        squares[0] = new Squares("Tower", +250, false);
-        squares[1] = new Squares("Crater", -100, false);
-        squares[2] = new Squares("Palace gates", +100, false);
-        squares[3] = new Squares("Cold Desert", -20, false);
-        squares[4] = new Squares("Walled City", +180, false);
-        squares[5] = new Squares("Monastery", 0, true);
-        squares[6] = new Squares("Black cave", -70, false);
-        squares[7] = new Squares("Huts in the mountain", +60, false);
-        squares[8] = new Squares("The Werewall (werewolf-wall)", -80, true);
-        squares[9] = new Squares("The pit", -50, false);
-        squares[10] = new Squares("Goldmine", +650, false);
+        squares[0] = new Squares("Climb the Tower and get 250$", +250, false);
+        squares[1] = new Squares("Oh no! You have stepped in a Crater and broke your leg. Pay 100$ for your medical bill", -100, false);
+        squares[2] = new Squares("You have entered the Palace gates and are rewarded with 100$", +100, false);
+        squares[3] = new Squares("BRRR it's cold in the desert, buy a pair of gloves for 20$", -20, false);
+        squares[4] = new Squares("Welcome to Walled City, you are rewarded with 180$", +180, false);
+        squares[5] = new Squares("Stay safe in the Monastery", 0, true);
+        squares[6] = new Squares("You have been robbed for 70$ in the Black cave", -70, false);
+        squares[7] = new Squares("You have reached the Huts in the mountain, claim your 60$", +60, false);
+        squares[8] = new Squares("You have been bit by a werewolf at The Werewall and need a vaccine for 80$)", -80, true);
+        squares[9] = new Squares("You have to fill a pit, buy a shovel for 50$", -50, false);
+        squares[10] = new Squares("You have found gold in the mountains and sell it for 650$", +650, false);
     }
 
-int getpoints (int dieValue) {
-    return valueSquare [dieValue -2];
-}
-public String getsquareName (int dieValue) {
+    int getpoints (int sumFacevalue) {
+        if (sumFacevalue == 12) {
+            return squares[0].getValue();
+        }
+        else {
+            return squares[sumFacevalue-1].getValue();
+        }
+    }
+    public String getText (int sumFacevalue) {
+        if (sumFacevalue == 12) {
+            return squares[0].getDescription();
+        }
+        else {
+            return squares[sumFacevalue-1].getDescription();
+        }
 
-    return squareName[dieValue-2];
+        boolean getBonusturn (int sumFacevalue) {
+            if (sumFacevalue == 12) {
+                return squares[0].getBonusturn();
+            }
+            else {
+                return squares[sumFacevalue-1].getBonusturn();
+            }
+        }
 
-}
+    }
 
 }
